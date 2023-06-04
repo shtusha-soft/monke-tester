b4b01b0223ec5607da90b4f6ff8cd58bb4220e1a
nataliia-d <82103446+nataliia-d@users.noreply.github.com>
2023-06-03 22:23:50 -0400

    yay movement!
    wow it works :')

--- Assets/Scripts/platformer/PlayerMovement.cs
+++ Assets/Scripts/platformer/PlayerMovement.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Animator animator;
+
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
     private bool isJumping = false;
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+
+            if (moveX < 0)
+        {
+            spriteRenderer.flipX = true; // Flips the sprite horizontally
+        }
+        else if (moveX > 0)
+        {
+            spriteRenderer.flipX = false; // Resets the sprite to its original orientation
+        }
+
+        animator.SetFloat("speed", Mathf.Abs(moveX));
 
         if (Input.GetButtonDown("Jump") && !isJumping)
         {
